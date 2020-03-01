0793896199065f6be76f8e278c11778f7e6d29fc
magneticgear <studio.electricsheep@gmail.com>
2020-03-01 13:41:57 +0900

    まだバグってるけどそのままマージ

--- burger_war/scripts/tofu.py
+++ burger_war/scripts/tofu.py
@@ -16,16 +16,25 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import numpy as np
 import random
 import sendIdToJudge
 
-class state():
-	def __init__(self, now=2, before=3):
-		self.now = now; self.before = before
-	def both_sub(self, x, y):#代入
-		self.now = x; self.before = y
-	def both_return(self):
-		return self.now, self.before
+class State():
+    '''
+    now:現在の状態
+    before:過去の状態
+    done:現在の状態が完了の有無のフラグ
+         False True
+    target_x,target_y:ターゲット座標
+    '''
+    def __init__(self, now=2, before=3, x=0, y=0):
+        self.now = now; self.before = before; self.done= False;
+        self.target_x = x; self.target_y = y
+    def both_sub(self, x, y, done):#代入
+        self.now = x; self.before = y; state.done = False
+    def both_return(self):
+        return self.now, self.before
 
 
 class TofuBot():
@@ -65,9 +74,58 @@ class TofuBot():
         twist = Twist()
         twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
         twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        self.vel_pub.publish(twist)
+
+
+    def calcTwist(self):
+    	x = 1
+	th = 0
+        twist = Twist()
+        twist.linear.x = x; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th
+        return twist
+
+    def extentFlag(self, x, lower_x, upper_x):
+        if lower_x < x and x < upper_x:
+            return True
+        return False
+
+    def area(self,img):
+	hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+	hsvLower = np.array([0, 128, 0])
+	hsvUpper = np.array([30, 255, 255])
+	mask1 = cv2.inRange(hsv, hsvLower, hsvUpper)
+
+	hsvLower = np.array([150, 128, 0])
+	hsvUpper = np.array([179, 255, 255])
+	mask2 = cv2.inRange(hsv, hsvLower, hsvUpper)
+	
+	mask = mask1 + mask2
+
+	masked_hsv = cv2.bitwise_and(img, img, mask=mask)
+	gray = cv2.cvtColor(masked_img,cv2.COLOR_BGR2GRAY)
+
+	ret,thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
+	image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	return cv2.contourArea(contours[0])
 
 
     def strategy(self):
+        state = State()
+        position = data.pose.pose.position
+        errbar = 0.2	#[m]
+        ex_state = [1,2,3,4]	#existing state
+	
+        '''
+        state = 1 壁沿い       
+        state = 2 原点復帰
+        state = 3 マーカに向かう+戻る
+        state = 4 止まる（enemyがcameraで見えている）
+        '''
+
+
         r = rospy.Rate(1) # change speed 1fps
 
         target_speed = 0
@@ -77,20 +135,50 @@ class TofuBot():
         r.sleep()
 
         while not rospy.is_shutdown():
-            twist = self.walltrace(1)
-            #twist = self.calcTwist()
-            #print(twist)
-            #print(self.laser)
+            x = position.x; y = position.y
+            area = area(self.img)
+            if area > 2000:
+			    state.both_sub(ex_state[3],state.now,False)
 
-            self.vel_pub.publish(twist)
-            
+            if state.now == ex_state[0]:
+                tracewall()
+                #的の近く
+                if extentFlag(x, x-errbar,x+errbar) and extentFlag(y, y-errbar,y+errbar):
+                    aimmarker()
+                    #元の位置に戻る
+                elif self.scan.ranges[0] < errbar:
+                    orgset()
+
+                if state.done == True:
+                    if state.before == ex_state[1]:
+                        state.both_sub(ex_state[2], ex_state[1], False)
+                    elif state.before == ex_state[2]:
+                        #前状態3->現在状態1
+                        state.both_sub(ex_state[1], ex_state[2], False)
+
+            elif state.now == ex_state[1]:
+                orgset()
+                if state.done == True:
+                    state.both_sub(ex_state[2], ex_state[1], False)
+            elif state.now == ex_state[2]:
+                aimmarker()
+                if state.done ==True:
+                    state.both_sub(ex_state[0], ex_state[2], False)
+
+            #割込みでカメラ情報を読んで止まる
+            elif state.now == ex_state[3]:
+                stop()
+                if state.done == True:
+                    state.both_sub(state.now, ex_state[3], False)
+#		else:
             r.sleep()
+#end while
 
     # lidar scan topic call back sample
     # update lidar scan state
     def lidarCallback(self, data):
         self.scan = data
-        #rospy.loginfo(self.scan.ranges)
+        rospy.loginfo(self.scan.ranges)
 	    #print(self.scan.ranges[10])
         self.laser = self.scan.ranges
 
@@ -102,7 +190,6 @@ class TofuBot():
         except CvBridgeError as e:
             rospy.logerr(e)
 
-        #cv2.imshow("Image window", self.img)
         cv2.imshow("Image window", self.img)
         cv2.waitKey(1)
 
